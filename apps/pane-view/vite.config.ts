9df7a7c70b40a282c3bdf4cbb91e8db27b53e42c
Traydr <Traydr@protonmail.com>
2026-06-03 01:30:00 +0300

    add nitro

--- apps/pane-view/vite.config.ts
+++ apps/pane-view/vite.config.ts
@@ -1,13 +1,33 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        "@aws-sdk/client-s3",
+        "@aws-sdk/s3-request-presigner",
+        "@latch-works/media-storage",
+        "tslib",
+      ],
+    },
+  },
   resolve: {
     tsconfigPaths: true,
+  },
+  ssr: {
+    external: [
+      "@aws-sdk/client-s3",
+      "@aws-sdk/s3-request-presigner",
+      "@latch-works/media-storage",
+      "tslib",
+    ],
   },
   server: {
     port: 3000,
   },
-  plugins: [tanstackStart(), viteReact()],
+  plugins: [nitro(), tanstackStart(), viteReact()],
+  envPrefix: "VITE_",
 });
