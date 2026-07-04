7457e56d17a69106557add737c63a1b537852d51
Traydr <Traydr@protonmail.com>
2026-07-04 06:33:57 +0100

    update with new icons

--- apps/pane-view/src/routes/__root.tsx
+++ apps/pane-view/src/routes/__root.tsx
@@ -30,7 +30,9 @@ export const Route = createRootRouteWithContext<{
       },
     ],
     links: [
-      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+      { rel: "icon", href: "/favicon.ico", sizes: "any" },
+      { rel: "icon", href: "/favicon.png", type: "image/png" },
+      { rel: "apple-touch-icon", href: "/apple-touch-icon.png" },
       { rel: "stylesheet", href: appCss },
     ],
   }),
